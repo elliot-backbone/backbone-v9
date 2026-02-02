38d04521575433560ba71b75db0c6a16a8b67789
elliot-backbone <elliot@backboneam.com>
2026-02-02 19:05:54 +0000

    Add vertical burger nav + horizontal top nav for entity browsing - ui/pages/_app.js

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,11 +1,5 @@
 import '../styles/globals.css'
-import EntitySearch from '../components/EntitySearch'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <EntitySearch />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
